fe80cc8c8cdf0df7b9bdbdbd4b77385a76407f97
JLshanmathi <jl.shanmathi@gmail.com>
2023-11-27 15:56:15 +0530

    Task done

---
+++ script.js
@@ -0,0 +1,34 @@
+
+setTimeout(function () {
+document.getElementById('display').innerHTML = 10;
+setTimeout(function () {
+document.getElementById('display').innerHTML = 9;
+setTimeout(function () {
+document.getElementById('display').innerHTML = 8;
+setTimeout(function () {
+document.getElementById('display').innerHTML = 7;
+setTimeout(function () {
+document.getElementById('display').innerHTML = 6;
+setTimeout(function () {
+document.getElementById('display').innerHTML = 5;
+setTimeout(function () {
+document.getElementById('display').innerHTML = 4;
+setTimeout(function () {
+document.getElementById('display').innerHTML = 3;
+setTimeout(function () {
+document.getElementById('display').innerHTML = 2;
+setTimeout(function () {
+document.getElementById('display').innerHTML = 1;
+setTimeout(function () {
+document.getElementById('display').innerHTML ='Happy Independence Day!';
+}, 1000);
+}, 1000);
+}, 1000);
+}, 1000);
+}, 1000);
+}, 1000);
+}, 1000);
+}, 1000);
+}, 1000);
+}, 1000);
+}, 1000);
